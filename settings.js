01c5dba9fc79762f99999ffd1cbb98fa9e22eba9
Piker12 <moussaba6452@gmail.com>
2025-02-20 12:04:47 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "IA KING",
-  botOwner: 'PIKER', // Your name
+  botName: "knighbot",
+  botOwner: 'knignbot', // Your name
   ownerNumber: '22664529962', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
